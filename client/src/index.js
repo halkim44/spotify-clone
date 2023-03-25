058778a6dd64b50514ec5de9c82ee5837c1599f9
Halkim Bin Rajan <halkimrajan@gmail.com>
2023-03-26 00:53:50 +0800

    used hashrouter for gh-pages

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { UserDataProvider } from "./contexts/userData";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <UserDataProvider>
       <App />
     </UserDataProvider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
 
